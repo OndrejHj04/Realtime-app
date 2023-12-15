433a2a5e98696c2ee99375c14d75a2bc2b5aba0e
OndrejHj04 <ondra.hajku@seznam.cz>
2023-12-15 17:34:19 +0100

    ui styled!

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 1rem;
+  border-bottom: 1px solid #ccc;
+`;
+
 const H1 = styled.h1`
   font-size: 2rem;
 `;
 
 function Header() {
   return (
-    <div>
+    <Container>
       <H1>piškvorky</H1>
-    </div>
+    </Container>
   );
 }
 export default Header;
